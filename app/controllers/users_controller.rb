1b1707779ee5d99a81100bc2eda1919dee4f21a5
Ktsunezawa <rapurasnohako@gmail.com>
2021-04-27 06:23:23 +0000

    Add User Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @books = Book.all
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+       redirect_to users_path
+    else
+      render edit
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    require(:user).permit(:name, :introduce, :profile_image)
+  end
 end
