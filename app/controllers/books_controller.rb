984ea07dde117df29fcbc2e400e21d180701221c
Ktsunezawa <rapurasnohako@gmail.com>
2021-04-28 03:50:15 +0000

    Add Book Show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
   end
 
   def edit
@@ -18,7 +20,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-       redirect_to books_path
+       redirect_to book_path(@book.id)
     else
       render new
     end
