12b991b05394b8d55958abf972b1d4c8e9bc6339
Ktsunezawa <rapurasnohako@gmail.com>
2021-05-04 05:11:39 +0000

    Add Favorite

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, except: [:new, :create, :destroy]
   resources :books, except: [:new] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 end
