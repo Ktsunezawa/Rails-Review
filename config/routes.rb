3cb304dfb6e190cecff11fb11884b67ede3574e5
Ktsunezawa <rapurasnohako@gmail.com>
2021-05-06 03:48:24 +0000

    Add Follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users, except: [:new, :create, :destroy]
+  resources :users, except: [:new, :create, :destroy] do
+    resource :relationships, only: [:create, :destroy]
+    get 'relationships/followings' => 'relationships#followings', as: 'followings'
+    get 'relationships/followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, except: [:new] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
